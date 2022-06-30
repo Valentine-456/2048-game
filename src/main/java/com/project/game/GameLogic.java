742bbb22dfa774a39704b0c022bc95fb33252f02
Daniil Kucherenko <daniil.ukraine@gmail.com>
2022-06-30 15:49:10 +0300

    GameLogic updated, still need to be finished

--- src/main/java/com/project/game/GameLogic.java
+++ src/main/java/com/project/game/GameLogic.java
@@ -5,6 +5,7 @@ import com.project.game.keys.Directions;
 public class GameLogic {
     private Score score = new Score();
     private Tile tileObj = new Tile();
+    private int numOfTurns = 0;
     private int gameBoardSize;
     private Tile board[][] = new Tile[gameBoardSize][gameBoardSize];
 
@@ -13,6 +14,37 @@ public class GameLogic {
     }
 
     public void gameTurn(Directions direction) {
+        if (checkSameTilesInRow())
+            tileObj.mergeWithPrev(direction);
+        moveTiles();
+        updateScore();
+        if (checkEmptyBoxes())
+            addRandomTile();
+        numOfTurns++;
 
+    }
+
+    private void addRandomTile() {}
+
+    private boolean checkSameTilesInRow() {
+        for (int row = 0; row < gameBoardSize; row++) {
+            for (int col = 0; col < gameBoardSize; col++) {
+                if (board[row][col] == board[row][col + 1])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private void moveTiles(){}
+    private void updateScore(){}
+    private boolean checkEmptyBoxes(){
+        for (int row = 0; row < gameBoardSize; row++) {
+            for (int col = 0; col < gameBoardSize; col++) {
+                if (board[row][col] == null)
+                    return true;
+            }
+        }
+        return false;
     }
 }
